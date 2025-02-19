d9ffafd3a48f6deefaa7d860af33e708013deffc
Pranitalokhande1 <pranital571@gmail.com>
2025-02-19 10:39:38 +0530

    file updated

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages
+
+
+
+setup(
+name="Xray",
+version="0.0.1",
+author="sunny svita"
+author_email="sunny.svita@ineron.ai",
+install_requires=[]
+package=find_packages()
+
+
+)
